c0163cc2137aba36e412115bb02c30e9ddbbfb7c
Steve Harsant <stevenharsant@gmail.com>
2021-06-06 13:50:39 +0100

    added env var support, cleanup, etc

--- README.md
+++ README.md
@@ -2,18 +2,31 @@
 
 A shell script to manage cloudflare DNS via their rest API
 
-version: `0.2.3` (semver)
+version: `0.3.0` (semver)
 
 ## Usage
 
 * Clone the repository.
-* Ensure the `settings.conf` file is found in the same directory as the script.
-* Update the settings in the `settings.conf` file
-* It is recommended to ensure the `settings.conf` file has the permissions set to 600. `chmod 600 settings.conf`
 * Ensure the script is executable. `chmod +x cloudflare-ddns-updater.sh`
+* Set the envionment variables (detailed below) to your domain/account details OR create a `settings.conf` in the same directory as the script (example syntax below).
+  * If using the `settings.conf` file, it is recommended to ensure the file has the permissions set to 600. `chmod 600 settings.conf`
 * The script can be run manually, or use cron to run it on a schedule.
-  - To schedule with cron, open crontab with `crontab -e`
-  - Paste in `*/10 * * * * /path/to/cloudflare-ddns-updater.sh` This will run the script every 10 minutes.
+  * To schedule with cron, open crontab with `crontab -e`
+  * Paste in `*/10 * * * * /path/to/cloudflare-ddns-updater.sh` This will run the script every 10 minutes.
+
+> **NOTE:** The `settings.conf` takes precedence over environment variables. This is useful if multiple instances of the script is needed.
+
+## Environment Variables
+
+| Name         | Example Value                          | Required | Fallback value                         |
+|--------------|----------------------------------------|----------|----------------------------------------|
+| api_endpoint | <https://api.cloudflare.com/client/v4> | False    | <https://api.cloudflare.com/client/v4> |
+| api_key      | 1234567890987654321                    | True     | None                                   |
+| domain       | example.com                            | True     | None                                   |
+| email        | me@email.com                           | True     | None                                   |
+| enable_debug | `0` OR `1`                             | False    | 0                                      |
+| ttl          | 3600                                   | False    | 120                                    |
+| zone_id      | 123abc456xzy123abc456xzy               | True     | None                                   |
 
 ## Example settings file
 
@@ -24,9 +37,14 @@ email=me@email.com
 zone_id=123abc456xzy123abc456xzy
 ```
 
-## Roadmap
+## Changelog
 
-* Better error handling
-* Logging
-* Support for multiple hostnames
-* Support for more than just getting and updating A records
+### 0.2.3
+
+* First release
+
+### 0.3.0
+
+* Added environment variable functionality
+* Cleanup up script
+* Added logging
--- cloudflare-ddns-updater.sh
+++ cloudflare-ddns-updater.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Cloudflare DNS Manager
-# version: 0.2.3
+# version: 0.3.0
 # Author: Steve Harsant
 
 # Set liniting rules
@@ -9,27 +9,16 @@
 # shellcheck disable=SC2091
 # shellcheck disable=SC2164
 
-api_endpoint='https://api.cloudflare.com/client/v4'
-ttl="120"
+# Set variables from env vars, otherwise fallback to defaults
+api_endpoint=${CLOUDFLARE_API_ENDPOINT:-https://api.cloudflare.com/client/v4}
+api_key=$CLOUDFLARE_API_KEY
+domain=$CLOUDFLARE_DOMAIN
+email=$CLOUDFLARE_EMAIL
+enable_debug=${ENABLE_DEBUG:-0}
+ttl=${CLOUDFLARE_DEFAULT_TTL:-120}
+zone_id=$CLOUDFLARE_ZONE_ID
 
-# Enable debug messages
-enable_debug=0
-
-get_dns_a_record() {
-  curl -sS -X GET "${api_endpoint}/zones/${4}/dns_records?type=A&name=${2}" \
-    -H "X-Auth-Email: ${3}" \
-    -H "X-Auth-Key: ${1}" \
-    -H "Content-Type: application/json"
-}
-
-update_dns_a_record() {
-  curl -sS -X PUT "${api_endpoint}/zones/${4}/dns_records/${5}" \
-    -H "X-Auth-Email: ${3}" \
-    -H "X-Auth-Key: ${1}" \
-    -H "Content-Type: application/json" \
-    --data "{\"type\":\"A\",\"name\":\"${2}\",\"content\":\"${6}\",\"ttl\":${ttl},\"proxied\":false}"
-}
-
+# Script functions
 debug() {
   if [[ $enable_debug == 1 ]]; then
     printf "$1\n"
@@ -47,28 +36,63 @@ test_requirement() {
   fi
 }
 
-script_location="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Cloudflare API call functions
 
+get_dns_a_record() {
+  debug "Retrieving DNS A record from Cloudflare for domain: $2"
+  curl -sS -X GET "$api_endpoint/zones/$4/dns_records?type=A&name=$2" \
+    -H "X-Auth-Email: $3" \
+    -H "X-Auth-Key: $1" \
+    -H "Content-Type: application/json"
+}
+
+update_dns_a_record() {
+  debug "Updating DNS A record to: $6 in Cloudflare for domain: $2"
+  curl -sS -X PUT "$api_endpoint/zones/$4/dns_records/$5" \
+    -H "X-Auth-Email: $3" \
+    -H "X-Auth-Key: $1" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"$2\",\"content\":\"$6\",\"ttl\":$ttl,\"proxied\":false}"
+}
+
+#
+# Start
+#
+
+# Ensure requirements are satisfied
 test_requirement curl "$(command -v curl)"
-test_requirement settings.conf "$(ls "${script_location}"/settings.conf)"
 
-# Get configuration from settings file
-configuration=$(cat "${script_location}/settings.conf")
-for line in $configuration; do
-  case "$line" in
-  api_key*) api_key=$(parse_configuration "$line") ;;
-  domain*) domain=$(parse_configuration "$line") ;;
-  email*) email=$(parse_configuration "$line") ;;
-  zone_id*) zone_id=$(parse_configuration "$line") ;;
-  esac
-done
+# If not all environment variables are set, ensure the settings.conf file exists
+# If it exists, update with required values
+if [ -z "$api_key" ] || [ -z "$domain" ] || [ -z "$email" ] || [ -z "$zone_id" ]; then
+  script_location="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  test_requirement settings.conf "$(ls "$script_location"/settings.conf)"
+fi
+
+# Get configuration from settings file if it exists. This overwrites environment variables.
+# Useful if multiple instances of the script is needed.
+if [[ -f "$script_location/settings.conf" ]]; then
+  configuration=$(cat "$script_location/settings.conf")
+  for line in $configuration; do
+    case "$line" in
+    api_key*) api_key=$(parse_configuration "$line") ;;
+    domain*) domain=$(parse_configuration "$line") ;;
+    email*) email=$(parse_configuration "$line") ;;
+    zone_id*) zone_id=$(parse_configuration "$line") ;;
+    esac
+  done
+fi
 
 # Print debug messages of configruation variables
-debug " api_key: $api_key"
-debug " domain: $domain"
-debug " email: $email"
-debug " zone_id: $zone_id"
+debug "api_endpoint: $api_endpoint"
+debug "api_key: $api_key"
+debug "domain: $domain"
+debug "email: $email"
+debug "enable_debug: $enable_debug"
+debug "ttl: $ttl"
+debug "zone_id: $zone_id"
 
+# Get current DNS configuration details
 current_dns_configuration=$(get_dns_a_record "$api_key" "$domain" "$email" "$zone_id")
 current_recorded_ip=$(echo "$current_dns_configuration" | grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')
 desired_ip=$(curl -sS https://api.ipify.org)
@@ -88,7 +112,7 @@ debug " domain_id: $domain_id"
 
 # Test if DNS record is current
 if [[ "$current_recorded_ip" == "$desired_ip" ]]; then
-  printf "Cloudflare DNS entry matches current IP for ${domain}. exit 0 \n"
+  printf "Cloudflare DNS entry matches current IP for $domain. exit 0 \n"
   exit 0
 fi
 
@@ -96,7 +120,7 @@ update_result=$(update_dns_a_record "$api_key" "$domain" "$email" "$zone_id" "$d
 debug "$update_result"
 
 if echo "$update_result" | grep -q '\"success\": true,'; then
-  printf "Success! The IP address was updated to ${desired_ip}. exit 0 \n"
+  printf "Success! The IP address was updated to $desired_ip. exit 0 \n"
   exit 0
 else
   printf "Failure! IP address was not updated. exit 1 \n"
